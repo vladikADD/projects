23a7a76b535e21322e2392c1d53ae0610b2aabf7
vladikADD <seryj7905@gmail.com>
2025-01-30 17:04:34 +0300

    Правки контроллера и тестов (#3)

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'show' do
-    subject { get :show, params: { id: '6' } }
+    subject { json_response }
+
+    let!(:send_request) { get :show, params: { id: user.id } }
 
     let!(:user) { User.create(email: 'barabah@mail.ru', password: '1234') }
 
     let!(:data_expected) do
       {
+        id: user.id,
         email: 'barabah@mail.ru',
         password: '1234'
       }
